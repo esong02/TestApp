0cb307c3db7e7237700cbe0d53b6d1c6d3a919f0
WilliamMerritt <wmerritt@civi.ca>
2017-07-13 15:41:20 -0400

    Add project files.

---
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestApp
+{
+    
+	public partial class MainPage : ContentPage
+	{
+        
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+            
+
+            //syncDB.Clicked += MenuClickHandler;
+           // settings.Clicked += MenuClickHandler;
+            //myTasks.Clicked += MenuClickHandler;
+            //find.Clicked += MenuClickHandler;
+
+
+        }
+       
+        void MenuClickHandler(object sender, EventArgs args)
+        {
+            /*
+           Button menuBtn = sender as Button;
+
+           if (menuBtn == syncDB)
+           {
+               sampleLabel.Text = "Sync DB";
+           }else if (menuBtn == myTasks)
+           {
+               sampleLabel.Text = "My Tasks";
+           }else if (menuBtn == find)
+           {
+               sampleLabel.Text = "Find";
+           }else if (menuBtn == settings)
+           {
+               sampleLabel.Text = "Settings";
+           }
+           else
+           {
+               sampleLabel.Text = "Unknown";
+           }
+
+           */
+        }
+
+        private void TapSyncDB(object sender, EventArgs e)
+        {
+            sampleLabel.Text = "Sync DB";
+        }
+
+        private void TapMyTasks(object sender, EventArgs e)
+        {
+            sampleLabel.Text = "My Tasks";
+        }
+
+        private void TapFind(object sender, EventArgs e)
+        {
+            sampleLabel.Text = "Find";
+        }
+
+        private void TapSettings(object sender, EventArgs e)
+        {
+            sampleLabel.Text = "Settings";
+        }
+
+
+        /*
+        void OnLoginClicked(object sender, EventArgs args)
+        {
+
+            if (username.Text.Equals("Civica",StringComparison.OrdinalIgnoreCase))
+            {
+                if (password.Text.Equals("pass",StringComparison.OrdinalIgnoreCase))
+                {
+                    loginButton.Text = "Logged In!";
+                }
+                else
+                {
+                    loginButton.Text = "Wrong Password";
+                }
+            }
+            else
+            {
+                loginButton.Text = "Wrong Username";
+            }
+
+
+        }
+        */
+    }
+}
